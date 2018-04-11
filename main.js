5ce6af5f62e55b868f82449b28cfffa7b602b590
Jonathan Dahan <hi@jonathan.is>
2018-04-11 13:22:05 -0400

    first working verison

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var startExperienteBtn = document.getElementById('start_experience');
 
-startExperienteBtn.onclick = function(){
     document.getElementById('container').outerHTML = '';
     document.getElementsByTagName('a-scene')[0].style.zIndex = 'auto';
-};
